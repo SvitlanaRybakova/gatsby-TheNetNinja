fadf14a991c36161de78ae7294f4f7660a7f3a89
Svitlana <devop.edu@gmail.com>
2021-05-12 18:51:58 +0200

    init project

---
+++ my-gatsby-project/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>
+        About page
+      </h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem voluptates corporis id, a enim quam quis voluptate, dicta odio quo sint. Voluptate aliquid dolores reiciendis similique omnis nemo, est tenetur?</p>
+    </div>
+  )
+}
